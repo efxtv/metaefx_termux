d8fac4e7fa86ccb530caab347e444ddba8b89eeb
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2020-07-22 10:10:25 -0700

    Create metaefx_termux.sh

---
+++ metaefx_termux.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo $red 
+echo ' 
+         (\____/)
+          (_oo_)
+            (O)
+          __||__    \)
+       []/______\[] /
+       / \______/ \/
+      /    /__\
+     (\   /____\
+
+/....TuRn On yOur intErneT coNnecTion....\'
+echo " "
+echo "### This Script is written by EFX Tv###"
+echo " "
+echo "----------- Fetching.../// ---------️----"
+echo " "
+apt-get update -y
+apt-get upgrade -y
+pkg install ruby -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+echo " "
+echo "############# Completed ################"                                                                                                                       
+echo " "
+echo "If You Are Connected To Internet Then 
+you have downloaded metasploit successfully. 
+Subscribe us on youtube (EFX Tv) "
+echo " "
+echo " (x ...Hack The Planet... x) "
+echo " "
+echo '
+          \__/          Bye Bye guys...
+         `(  `^=_ p _###_
+          c   /  )  |   /
+   _____- //^---~  _c  3
+ /  ----^\ /^_\   / --,-
+(   |  |  O_| \\_/  ,/
+|   |  | / \|  `-- /
+(((G   |-----|
+      //-----\\
+     //       \\
+   /   |     |  ^|
+   |   |     |   |
+   |____|    |____|
+  /______)   (_____\'
+echo " "
+echo "Type msfconsole to start metasploit..."
+echo " "
+exit
